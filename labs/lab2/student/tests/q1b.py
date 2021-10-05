0511f3c44cdb72e6f2133fa6083f69ddf444add2
yidan sun <yidansun@ucsb.edu>
2021-10-04 21:02:59 -0700

    remove old student folder

--- labs/lab2/student/tests/q1b.py
+++
@@ -1,8 +0,0 @@
-test = {   'name': 'q1b',
-    'points': 3,
-    'suites': [   {   'cases': [   {'code': '>>> fruit_info["rank2"].dtype\ndtype(\'int64\')', 'hidden': False, 'locked': False},
-                                   {'code': '>>> all(fruit_info["rank2"] == fruit_info["rank1"])\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
