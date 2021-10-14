38e92407fce5ae7f6053004799d207016e303d6f
yidan sun <yidansun@ucsb.edu>
2021-10-14 12:17:59 -0700

    release lab4

---
+++ labs/lab4/student/tests/q1a.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1a',
+    'points': 3,
+    'suites': [   {   'cases': [{'code': ">>> 'categorical' in q1a\nTrue", 'hidden': False, 'locked': False}, {'code': ">>> 'quantitative' in q1a\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
