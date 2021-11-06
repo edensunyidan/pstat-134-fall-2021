dd6e5bf6951d3526030a9c39048abd13ffeea51c
yidan sun <yidansun@ucsb.edu>
2021-11-05 20:51:11 -0700

    release lab6

---
+++ labs/lab6/student/tests/q3.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q3',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> W.shape\n(1288, 200)', 'hidden': False, 'locked': False},
+                                   {'code': '>>> H.shape\n(200, 1850)', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.round(sum(H), 2)\narray([261.62, 276.08, 279.31, ..., 229.24, 229.8 , 232.48])', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
